3d0a2023f9eab4b3d21b8788d335c2abe7290419
uribaron28 <uribaron28@gmail.com>
2026-04-02 14:08:42 -0400

    Added distance lookup and trial order

--- starter_file_uri.py
+++ starter_file_uri.py
@@ -19,6 +19,7 @@ window_close = 18 #end of delivery window
 break_time = 10 #break time is 10 hours
 max_driving = 11 #11 hours max driving
 max_duty = 14 #14 hours on duty, driving + waiting + loading
+depot_zip = 1887 #depot zipcode
 
 #clean data
 orders = orders[orders["ORDERID"] != 0].copy()
@@ -42,9 +43,42 @@ distances["ZIPID"] = pd.to_numeric(distances["ZIPID"], errors="raise")
 distances = distances.set_index("ZIP")
 
 dist_matrix = distances.drop(columns=["ZIPID"]).copy()
-print("\nCleaned distance matrix:")
-print(dist_matrix.shape)
-print(dist_matrix.head())
 
 order_zips = set(orders["TOZIP"].unique())
 distance_zips = set(dist_matrix.index)
+
+def get_distance(zip1, zip2):
+    return dist_matrix.loc[zip1, zip2]
+
+
+# grab one example order
+sample_order = orders.iloc[0]
+
+print("\nSample order:")
+print(sample_order)
+
+order_id = sample_order["ORDERID"]
+dest_zip = sample_order["TOZIP"]
+cube = sample_order["CUBE"]
+day = sample_order["DayOfWeek"]
+
+print("\nParsed values:")
+print("Order ID:", order_id)
+print("Destination ZIP:", dest_zip)
+print("Cube:", cube)
+print("Day:", day)
+
+# distance and drive time from depot to this order
+miles = get_distance(depot_zip, dest_zip)
+drive_time = miles / driving_speed
+
+print("\nTravel info:")
+print("Miles from depot:", miles)
+print("Drive time (hours):", drive_time)
+
+# unload time for this order
+unload_time = max(min_time, unload_rate * cube)
+
+print("\nService info:")
+print("Unload time (minutes):", unload_time)
+print("Unload time (hours):", unload_time / 60)
