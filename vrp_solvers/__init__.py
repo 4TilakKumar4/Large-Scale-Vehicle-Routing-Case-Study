e90268ac63da446e4503d84011c9b152b7d1943f
Tilak Kumar <4tilak.kumar4@gmail.com>
2026-04-20 20:57:27 -0400

    Added Or tools implimentation because i was too lazy to merge and do it the right way

--- vrp_solvers/__init__.py
+++ vrp_solvers/__init__.py
@@ -1,35 +1,31 @@
 """
 vrp_solvers — VRP solution algorithm package for the NHG case study.
-
 Run VRP_DataAnalysis.py first to populate data/ before importing any solver.
-
 Usage:
     from vrp_solvers.base import loadInputs, DAYS
     from vrp_solvers.clarkeWright import ClarkeWrightSolver
     from vrp_solvers.tabuSearch import TabuSearchSolver
-
     orders, distMatrix = loadInputs()
     solver = ClarkeWrightSolver(useTwoOpt=True, useOrOpt=True)
     routes = solver.solve(dayOrders)
 """
 
 from vrp_solvers.base import loadInputs, evaluateRoute, DAYS
-
-from vrp_solvers.clarkeWright       import ClarkeWrightSolver
-from vrp_solvers.nearestNeighbor    import NearestNeighborSolver
-from vrp_solvers.tabuSearch         import TabuSearchSolver
-from vrp_solvers.simulatedAnnealing import SimulatedAnnealingSolver
-from vrp_solvers.alns               import ALNSSolver
-from vrp_solvers.resourceAnalyser   import ResourceAnalyser
-from vrp_solvers.costModel          import CostModel, SENSITIVITY_RANGES
+from vrp_solvers.clarkeWright        import ClarkeWrightSolver
+from vrp_solvers.nearestNeighbor     import NearestNeighborSolver
+from vrp_solvers.tabuSearch          import TabuSearchSolver
+from vrp_solvers.simulatedAnnealing  import SimulatedAnnealingSolver
+from vrp_solvers.alns                import ALNSSolver
+from vrp_solvers.resourceAnalyser    import ResourceAnalyser
+from vrp_solvers.costModel           import CostModel, SENSITIVITY_RANGES
+from vrp_solvers.orToolsSolver       import ORToolsSolver
 from vrp_solvers.relaxedScheduleSolver import (
     SweepRelaxedSolver,
     ALNSRelaxedSolver,
     getVisitGroups,
 )
-
-from vrp_solvers.mixedFleetSolver   import MixedFleetSolver, ALNSMixedFleetSolver
-from vrp_solvers.overnightSolver    import (
+from vrp_solvers.mixedFleetSolver    import MixedFleetSolver, ALNSMixedFleetSolver
+from vrp_solvers.overnightSolver     import (
     OvernightSolver,
     evaluateOvernightRoute,
     applyOvernightImprovements,
@@ -48,7 +44,7 @@ __all__ = [
     "ResourceAnalyser",
     "CostModel",
     "SENSITIVITY_RANGES",
-    "RelaxedScheduleSolver",
+    "ORToolsSolver",
     "SweepRelaxedSolver",
     "ALNSRelaxedSolver",
     "getVisitGroups",
@@ -58,4 +54,5 @@ __all__ = [
     "evaluateOvernightRoute",
     "applyOvernightImprovements",
     "findAllOvernightCandidates",
+    "RelaxedScheduleSolver"
 ]
